4a9b1958804310cfffc8d8ce975752eb26100682
Trevor Rice <trevor.rice@morningstar.com>
2020-10-25 20:05:17 -0500

    Add alert example

---
+++ examples/src/components/alert/example-assertive.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <button @click="setAlert">Trigger an alert!</button>
+    <alert v-for="(alert, index) in alerts" :key="index" type="assertive">
+      {{ alert }}
+    </alert>
+  </div>
+</template>
+
+<script>
+import Alert from '@plinth-ui/alert'
+
+export default {
+  components: {
+    Alert,
+  },
+  data() {
+    return {
+      alerts: [],
+    }
+  },
+  methods: {
+    setAlert() {
+      this.alerts.push(`Alert #${this.alerts.length + 1}`)
+    },
+  },
+}
+</script>
